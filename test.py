630cdae8f17c913d9549884399b4c5fccbf26e7a
Cihan Ceyhan <chncyhn@gmail.com>
2021-02-08 12:22:55 +0100

    Fix test formatting

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from anneal import SimAnneal
 import matplotlib.pyplot as plt
 import random
 
+
 def read_coords(path):
     coords = []
     with open(path, "r") as f:
@@ -10,11 +11,13 @@ def read_coords(path):
             coords.append(line)
     return coords
 
+
 def generate_random_coords(num_nodes):
     return [[random.uniform(-1000, 1000), random.uniform(-1000, 1000)] for i in range(num_nodes)]
 
+
 if __name__ == "__main__":
-    coords = read_coords("coord.txt") # generate_random_coords(100)
+    coords = read_coords("coord.txt")  # generate_random_coords(100)
     sa = SimAnneal(coords, stopping_iter=5000)
     sa.anneal()
     sa.visualize_routes()
